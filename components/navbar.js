a89e9d853a9aef22be3f754293507040a6a3260f
AJ Sabandal <annamariejsabandal@gmail.com>
2020-02-19 15:21:25 +0800

    change navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,23 @@
 export default function Navbar(){
  return (
-    <p> x </p>
+    <>
+        <div className="nav-right">
+            <div className="button" id="btn">
+                <div className="bar-top"></div>
+                <div className="bar-mid"></div>
+                <div className="bar-btm"></div>
+            </div>
+        </div>
+
+        <nav>
+            <div className="nav-right-hidden">
+                <div className="button" id="btn">
+                <div className="bar-top"></div>
+                <div className="bar-mid"></div>
+                <div className="bar-btm"></div>
+                </div>
+            </div>
+        </nav>
+    </>
  );
 }
