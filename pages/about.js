37210b6628d5a696fa5247466913f6e074dba9a1
annamariejsabandal@gmail.com <annamariejsabandal@gmail.com>
2020-09-24 14:46:21 +0800

    add resume link

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,10 @@ export default function AboutPage() {
           <div className="about-body">
             <p>hi! i’m aj sabandal from manila, philippines i graduated at polytechnic university of the philippines with a program of bachelor of business teacher education major in information technology, year 2017</p>
             <p>after i graduated i was a freelancer. i’m doing art commissions and at year 2018 i was hired at the hotel (the mansion iloilo) as a sales coordinator.  i left there on 2019 and attended full stack web dev bootcamp for 3 months at zuitt.</p>
-            <p>i really want to pursue programming. i want to work on environment where i can  learn more knowledge and grow as person. i want to do the best that i can to benefit the company and the team i will be part of.</p>
+            <p>as an Web Developer, It's challenging to me but if you love what you do. You will take every challenge. You take the risk. What I love on being a Web Developer is the learning curve. You learn everyday and this is also makes me happy especially when you fix something. Sometime it takes minutes or even an hour, it almost break you but answers will just pop up! just like that.</p>
+            <p>i really want to pursue programming. i want to work on environment where i can  learn more knowledge and grow as person. i want to do the best that i can to benefit the company and the team i will be part of.
+            </p>
+            <h3><a href="https://drive.google.com/file/d/12CqoDxJed7KFkkXSBwiBxP_Bhs7-WLxD/view?usp=sharing">Resume</a></h3>
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import Fontawesome from '../components/fontawesome'
 export default function IndexPage() {
     return (
       <>
-      <div className="text-content">
-          <h1>aj sabandal</h1>
-          <h2>full stack web developer using javascript and ruby</h2>
-          <h3><a href="https://www.linkedin.com/in/anna-marie-joy-sabandal-899209196/">linkedin |</a></h3>
-          <h3><a href="https://github.com/ajsabandal">github</a></h3>
-        </div>
-      <div className="arrow">
+        <div className="text-content">
+            <h1>aj sabandal</h1>
+            <h2>full stack web developer using javascript and ruby</h2>
+            <h3><a href="https://www.linkedin.com/in/anna-marie-joy-sabandal-899209196/">linkedin |</a></h3>
+            <h3><a href="https://github.com/ajsabandal">github</a></h3>
+          </div>
+        <div className="arrow">
 
       </div>
     </>
